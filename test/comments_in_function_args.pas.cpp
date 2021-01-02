34ff520f5de9032a32757a1ebdaae9e192b1a52a
Ed Beroset <beroset@ieee.org>
2021-01-02 13:53:16 -0500

    Second round of cleanup and code test fixes.

---
+++ test/comments_in_function_args.pas.cpp
@@ -0,0 +1,28 @@
+/** \file
+ * comments_in_function_args
+ */ 
+ 
+
+
+   	   	   
+Integer BlockCommentsInFunctionArgs (TList a ,/* of Integer*/
+TList b ,/*of String*//*and another annotation*/
+Integer c /*The end*/
+);
+
+
+       
+Integer LineCommentsInFunctionArgs (TList a ,///of Integer
+/// and another comment
+
+TVector b // and the second param
+
+);
+
+
+
+
+
+
+// finished
+
