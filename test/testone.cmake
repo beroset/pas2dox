29e8fe3c391a7c76f2edc56508b83c633fd16e53
Ed Beroset <beroset@ieee.org>
2021-01-03 12:10:39 -0500

    Fix tests to remove output file before comparing.

--- test/testone.cmake
+++ test/testone.cmake
@@ -1,3 +1,4 @@
 execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${TESTDIR}/${CMAKE_ARGV6}" "${CMAKE_ARGV6}")
+execute_process(COMMAND "${CMAKE_COMMAND}" -E rm "${CMAKE_ARGV6}.cpp")
 execute_process(COMMAND "${CMAKE_ARGV5}" -o "${CMAKE_ARGV6}")
 execute_process(COMMAND "${CMAKE_COMMAND}" -E compare_files "${TESTDIR}/${CMAKE_ARGV6}.cpp" "${CMAKE_ARGV6}.cpp")
